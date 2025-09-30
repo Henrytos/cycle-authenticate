09ceb882669131c7ca893196bcbb293dd8cf6242
Henry <hfarcaya@latam.stefanini.com>
2025-09-30 12:19:06 -0300

    chore: configuração dos arquivos dockerfile/docker-compose/.dockerignore e testContainers no projeto

--- src/test/java/com/stefanini/cycle_authenticate/TestcontainersConfiguration.java
+++ src/test/java/com/stefanini/cycle_authenticate/TestcontainersConfiguration.java
@@ -1,8 +1,17 @@
 package com.stefanini.cycle_authenticate;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer(){
+        return  new PostgreSQLContainer<>("postgres");
+    }
 
 }

186e20c53a42879bfe32d0f1910302dd506ca313
Henry <hfarcaya@latam.stefanini.com>
2025-10-10 13:18:12 -0300

    docs: documentando nova funcionalidade de perfil logado

--- src/main/java/com/stefanini/cycle_authenticate/application/ports/inbound/services/dtos/CreateUserBodyDTO.java
+++ src/main/java/com/stefanini/cycle_authenticate/application/ports/inbound/services/dtos/CreateUserBodyDTO.java
@@ -11,28 +11,29 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
 public record CreateUserBodyDTO(
-        @Schema(defaultValue = "jhon_doe", example = "jhon_doe", required = true)
+        @Schema(defaultValue = "jhon_doe", example = "jhon_doe")
         @NotBlank(message = "not blank username")
         String username,
 
-        @Schema(defaultValue = "jhon_doe@example.com", example = "jhon_doe@example.com", required = true)
+        @Schema(defaultValue = "jhon_doe@example.com", example = "jhon_doe@example.com")
         @Email(message = "email invalid")
         @NotBlank(message = "not blank email")
         String email,
 
-        @Schema(defaultValue = "Jhon_doe2006@2025", example = "Jhon_doe2006@2025", required = true,
+        @Schema(defaultValue = "Jhon_doe2006@2025", example = "Jhon_doe2006@2025",
                 description = "password must contain a special character, uppercase or lowercase and a maximum of 20 characters",
                 maxLength = 20)
         @NotBlank(message = "not blank password")
         @Size(max = 20)
         String password,
 
-        @Schema(defaultValue = "2006-01-01", example = "2006-01-01", required = true)
+        @Schema(defaultValue = "2006-01-01", example = "2006-01-01")
         @NotNull(message = "date of birth required")
         LocalDate dateOfBirth,
 
         @NotNull
         @ValueOfEnum(enumClass = UserRole.class, message = "deveria ser ROLE_USER ou ROLE_ADMIN")
+        @Schema(examples = {"ROLE_USER", "ROLE_ADMIN"})
         String userRole
 ) {
 }
--- src/main/java/com/stefanini/cycle_authenticate/infra/http/presenters/GetProfilePresenter.java
+++ src/main/java/com/stefanini/cycle_authenticate/infra/http/presenters/GetProfilePresenter.java
@@ -2,13 +2,18 @@ package com.stefanini.cycle_authenticate.infra.http.presenters;
 
 import com.stefanini.cycle_authenticate.domain.entities.User;
 import com.stefanini.cycle_authenticate.domain.value_objects.UserRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 
 public record GetProfilePresenter(
+        @Schema(example = "jhon_doe")
         String username,
+        @Schema(example = "jhon_doe@example.com")
         String email,
+        @Schema(examples = {"ROLE_USER", "ROLE_ADMIN"})
         UserRole userRole,
+        @Schema(example = "2006-01-01")
         LocalDate dateOfBirth) {
 
     public static GetProfilePresenter toHttp(User user) {

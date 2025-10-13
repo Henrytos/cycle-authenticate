874a4ca42b2231e8fafb8acff576d2d1d36031bc
Henry <hfarcaya@latam.stefanini.com>
2025-10-13 09:56:27 -0300

    style: realizando estilização tela de login

---
+++ front-end/src/app/pages/login/login.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterOutlet, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+
+    this.loginForm = formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      }
+    );
+  }
+
+  public loginFormSubmit() {
+    const { email, password } = this.loginForm.value;
+
+    if (this.loginForm.valid)
+      this.loginForm.reset()
+
+  }
+}
